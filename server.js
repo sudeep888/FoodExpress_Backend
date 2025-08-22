630231740fb8fb2ec695a27d210164b128f197ea
sudeep888 <sudeepdas0711@gmail.com>
2025-08-23 04:29:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
     
     // List of allowed origins
     const allowedOrigins = [
-      'http://localhost:3003',
+      // 'http://localhost:3003',
       'https://food-express-frontend-henna.vercel.app/', // Your Vercel URL
       process.env.FRONTEND_URL
     ].filter(Boolean);
